51cfef08424feb5d7822fcf3397875d334d2ffef
deepak-asai <deepak.a.1996@gmail.com>
2017-04-05 21:41:00 +0530

    new

--- source code/Life.java
+++ source code/Life.java
@@ -1,17 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package life;
-
-/**
- *
- * @author deepak
- */
 import java.io.*;
 import java.util.*;
-public class Life {
+class Life {
 
     
 	private int m,n;
